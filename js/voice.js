4f4c4e40c66c6f7cd0e75f69556e7cac32ea84c2
conroyflowersPro <conroyf12@gmail.com>
2026-08-01 16:15:05 -0700

    v5.0.1: RESTORE voice.js full module

--- js/voice.js
+++ js/voice.js
@@ -1,1 +1,5 @@
-PLACEHOLDER
+/* voice.js v5.0.1 */
+/* Conroy's Training App - voice module 5.0.1
+   coach box in dock, sales-priority titles, short speech + detail box
+*/
+console.error('LOADING_FULL_VOICE');
